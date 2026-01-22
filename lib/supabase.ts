6da410881fb899b08a9d81d241df36ad50b45e81
custodio-mozdamas <juniorcustodio301@gmail.com>
2026-01-22 10:23:35 +0200

    Delete lib directory

--- lib/supabase.ts
+++
@@ -1,7 +0,0 @@
-
-import { createClient } from '@supabase/supabase-js';
-
-const SUPABASE_URL = 'https://pdgxguxtiuvtbaotczgj.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_oraQhI-Kd45itDWbAAoDNQ_U1GSjTXp';
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
